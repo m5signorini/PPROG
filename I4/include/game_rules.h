52fc6b8c57163f96e36d8e9d343029740377b8c3
cesarramirez3 <48729142+cesarramirez3@users.noreply.github.com>
2019-12-12 20:16:36 +0100

    game_rules updated

--- I4/include/game_rules.h
+++ I4/include/game_rules.h
@@ -17,9 +17,19 @@
 #include "game.h"
 #include "die.h"
 
+#define N_RULES 17
+
 typedef struct _GameRules GameRules;
 
 GameRules* game_rules_create();
 STATUS game_rules_destroy(GameRules* game_rules);
+
+STATUS game_rules_dark(GameRules* game_rules);
+STATUS game_rules_clopen_links(GameRules* game_rules);
+STATUS game_rules_hide_objects(GameRules* game_rules);
+STATUS game_rules_drop_objects(GameRules* game_rules);
+STATUS game_rules_teleport(GameRules* game_rules):
+STATUS game_rules_main(GameRules* game_rules);
+
 
 #endif
