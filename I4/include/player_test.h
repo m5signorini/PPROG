47dece6ae544da5770b3defed4fec6696162abff
cesarramirez3 <48729142+cesarramirez3@users.noreply.github.com>
2019-12-13 15:06:11 +0100

    player_test added

---
+++ I4/include/player_test.h
@@ -0,0 +1,27 @@
+/**
+ * @brief It declares the tests for the PLAYER module
+ *
+ * @file PLAYER_test.h
+ * @author Profesores Pprog
+ * @version 2.0
+ * @date 19-01-2016
+ * @copyright GNU Public License
+ */
+
+#ifndef PLAYER_TEST_H
+#define PLAYER_TEST_H
+
+void test_player_create();
+void test_player_set_name();
+void test_player_inventory_set_max();
+void test_player_set_location();
+void test_player_add_object();
+void test_player_delete_object();
+void test_player_get_name();
+void test_player_inventory_get_max();
+void test_player_get_location();
+void test_player_get_id();
+void test_player_get_object_at();
+void test_player_get_number_objects();
+
+#endif
