8adaa4a0e2511de3fde97d0628dc9ff49b954c81
cesarramirez3 <48729142+cesarramirez3@users.noreply.github.com>
2019-12-13 20:20:25 +0100

    game test

---
+++ I4/include/game_test.h
@@ -0,0 +1,34 @@
+/**
+ * @brief It declares the tests for the game module
+ *
+ * @file space_test.h
+ * @author Profesores Pprog
+ * @version 2.0
+ * @date 19-01-2016
+ * @copyright GNU Public License
+ */
+
+#ifndef GAME_TEST_H
+#define GAME_TEST_H
+void
+void test_game_create();
+void test_game_update();
+void test_game_add_space();
+void test_game_get_space();
+void test_game_get_space_id_at();
+void test_game_get_space_at();
+void test_game_set_player();
+void test_game_get_player();
+void test_game_add_object();
+void test_game_get_object();
+void test_game_get_object_id_at();
+void test_game_get_object_at();
+void test_game_get_object_location();
+void test_game_set_die();
+void test_game_get_die();
+void test_game_add_link();
+void test_game_get_link();
+void test_game_get_link_id_at();
+void test_game_get_link_at();
+
+#endif
