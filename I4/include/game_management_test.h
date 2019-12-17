c7a3b90a3f3bdca17ab0d05dad9ff7984aeb5a77
cesarramirez3 <48729142+cesarramirez3@users.noreply.github.com>
2019-12-17 11:58:27 +0100

    game_mangement test

---
+++ I4/include/game_management_test.h
@@ -0,0 +1,19 @@
+/**
+ * @brief It declares the tests for the object module
+ *
+ * @file space_test.h
+ * @author Profesores Pprog
+ * @version 2.0
+ * @date 19-01-2016
+ * @copyright GNU Public License
+ */
+
+#ifndef GAME_MANAGEMENT_TEST_H
+#define GAME_MANAGEMENT_TEST_H
+
+
+void test_game_management_create_from_file(char* filename);
+void test_game_management_save(char* filename);
+void test_game_management_load_from_file(char* filename);
+
+#endif
